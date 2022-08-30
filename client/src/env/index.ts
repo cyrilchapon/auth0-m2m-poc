f9dda8b4e80598c538138fa7f236d37c56b83e6f
Cyril CHAPON <cyril@permettezmoideconstruire.fr>
2022-08-31 01:20:50 +0200

    Add env

--- client/src/env/index.ts
+++ client/src/env/index.ts
@@ -1,21 +1,24 @@
-import { cleanEnv, str, CleanedEnvAccessors } from 'envalid'
+import { cleanEnv, str, CleanedEnvAccessors, url } from 'envalid'
 
 const knownNodeEnv = ['development', 'production', 'test'] as const
 type KnownNodeEnv = typeof knownNodeEnv[number]
 
 interface AppEnv {
   NODE_ENV: KnownNodeEnv
+  API_URL: string
 }
 
 type RawEnvVal = NodeJS.ProcessEnv[string]
 
 const getAppEnv = () => {
   const env: Record<keyof AppEnv, RawEnvVal> = {
-    NODE_ENV: process.env.NODE_ENV
+    NODE_ENV: process.env.NODE_ENV,
+    API_URL: process.env.API_URL
   }
 
   const cleanedEnv = cleanEnv<AppEnv>(env, {
-    NODE_ENV: str({ choices: knownNodeEnv })
+    NODE_ENV: str({ choices: knownNodeEnv }),
+    API_URL: url()
   })
 
   return { ...cleanedEnv } as AppEnv & CleanedEnvAccessors
